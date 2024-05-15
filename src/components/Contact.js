6e10c064d6b2b653fd4099aaf3d858933273a9c5
DioverDM <97247851+dioverdm@users.noreply.github.com>
2024-05-14 21:35:37 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,7 +35,7 @@ function ContactForm() {
           <span></span>
           <span></span>
         </div>
-        <div className="title">Get in touch!</div>
+        <div className="title">¡Ponerse en contacto!</div>
         <form name="contact" method="POST" data-netlify-recaptcha="true">
           <input type="hidden" name="form-name" value="contact" />
 
@@ -43,7 +43,7 @@ function ContactForm() {
           <div className="input-container ic1">
             <label className="form-label" htmlFor="name"></label>
             <input className="form-control input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}
-              placeholder="Name"
+              placeholder="Nombre"
               required />
           </div>
 
@@ -51,7 +51,7 @@ function ContactForm() {
           <div className="input-container ic2">
             <label className="form-label" htmlFor="email"></label>
             <input className="form-control input"  type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
+              placeholder="Correo electrónico"
               required/>
           </div>
 
@@ -59,13 +59,13 @@ function ContactForm() {
           <div className="input-container ic2">
             <label className="form-label" htmlFor="message"></label>
             <textarea className="form-control input from-textarea"  id="message" name="message" value={message} onChange={(e) => setMessage(e.target.value)}
-            placeholder="Message"
+            placeholder="Mensaje"
             required />
           </div>
           <div data-netlify-recaptcha="true" className="reCaptcha"></div>
           {/* Submit */}
           <button className="btn btn-danger submit" type="submit" style={{ position: "relative", marginTop: "68px" }}>
-            Submit
+            Envíar
           </button>
         </form>
       </div>
